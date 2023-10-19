246c764d23d29abe574f97199e3f0f180a420ee8
Sigit Prabowo <sigit.prabowo@go-jek.com>
2023-10-19 18:30:11 +0700

    update proper data type

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,34 @@ use s2::cellid::CellID;
 use s2::latlng::LatLng;
 use s2::s1::Deg;
 
+fn parse_string_to_u64(s: &str) -> Result<u64, std::num::ParseIntError> {
+    s.parse::<u64>()
+}
+
 #[wasm_bindgen]
-pub fn calculate(lat: f64, lng: f64, lvl: i64) -> String {
+pub fn calculate(lat: f64, lng: f64, lvl: i32) -> String {
     let lat_lng = LatLng::new(Deg(lat).into(), Deg(lng).into());
     let ci = CellID::from(lat_lng).parent(lvl as u64);
     ci.0.to_string()
 }
 
 #[wasm_bindgen]
-pub fn get_coordinates(cellid: i64) -> String {
-    let ci = CellID(cellid as u64);
-    if !ci.is_valid() {
-        return "".to_string()
+pub fn get_coordinates(cellid: &str) -> String {
+    match parse_string_to_u64(&cellid) {
+        Ok(parsed_cellid) => {
+            let ci = CellID(parsed_cellid);
+            if !ci.is_valid() {
+                return "".to_string()
+            }
+            let points = Cell::from(ci).vertices();
+            let coordinates: Vec<String> = points.iter().map(|p| format!("{},{}", p.longitude().deg(), p.latitude().deg())).collect();
+            coordinates.join(";")
+        }
+        Err(_e) => {
+            // Failed to parse the string as u64
+            return "".to_string()
+        }
     }
-    let points = Cell::from(ci).vertices();
-    let coordinates: Vec<String> = points.iter().map(|p| format!("{},{}", p.longitude().deg(), p.latitude().deg())).collect();
-    coordinates.join(";")
 }
 
 #[cfg(test)]
@@ -33,11 +45,11 @@ mod tests {
 
     #[test]
     fn it_success_get_coordinates() {
-        assert_eq!("106.80603880529831,-6.238318259447594;106.81783770824106,-6.237933179178703;106.81783770824107,-6.227712965171154;106.80603880529831,-6.228097424504746", get_coordinates(3344469575738589184));
+        assert_eq!("106.80603880529831,-6.238318259447594;106.81783770824106,-6.237933179178703;106.81783770824107,-6.227712965171154;106.80603880529831,-6.228097424504746", get_coordinates("3344469575738589184"));
     }
 
     #[test]
     fn it_failed_get_coordinates() {
-        assert_eq!("", get_coordinates(0));
+        assert_eq!("", get_coordinates("0"));
     }
 }
